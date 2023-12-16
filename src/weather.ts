0e605f0b7e4aec6b1e430e7f00f06bd6cc437ffe
Hilton Shumway <hillshum@gmail.com>
2023-12-15 21:41:07 -0600

    Implement cachewarmer on weather

--- src/weather.ts
+++ src/weather.ts
@@ -1,44 +1,83 @@
 import axios from "axios";
 import {utcToZonedTime} from 'date-fns-tz';
+import Cache from "./parkCache";
 
 import locations from './disneyLocations.json';
 
 const apiKey = 'af79d4b39a7b47b9b44175548230912';
 
+interface Location {
+    city: string;
+    latitude: number;
+    longitude: number;
+}
 interface WeatherResult {
     temperature: number;
     conditions: number;
     time: number;
 }
 
-async function getWeather(latitude: number, longitude: number): Promise<WeatherResult> {
+interface WeatherCacheEntry {
+    temperature: number;
+    conditions: number;
+    tzString: string;
+}
+
+const weatherCache = new Cache<WeatherCacheEntry>(60);
+
+async function getWeatherFromApi(latitude: number, longitude: number): Promise<WeatherCacheEntry> {
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}&units=imperial`;
-
     try {
+
+        console.log(`fetching data from ${apiUrl}`)
         const response = await axios.get(apiUrl);
-        const { current } = response.data;
-        const temperature = current.temp_f;
-        const conditions = current.condition.code;
+        return {
+            temperature: response.data.current.temp_f,
+            conditions: response.data.current.condition.code,
+            tzString: response.data.location.tz_id,
+        };
 
-        const tzString = response.data.location.tz_id;
-
-        const currentTime = utcToZonedTime(new Date(), tzString);
-        const time = Math.trunc(currentTime.getTime() / 1000); // convert millis to seconds
-
-
-        return { temperature, conditions, time };
     } catch (error) {
         throw new Error(`Error fetching data from weatherapi.com: ${error.message}`);
     }
 }
 
+async function getWeather(location: Location): Promise<WeatherResult> {
+
+    let weather = weatherCache.get(location.city);
+
+    if (!weather) {
+        console.log(`cache miss on ${location.city}`)
+        weather = await getWeatherFromApi(location.latitude, location.longitude)
+        weatherCache.set(location.city, weather);
+    }
+
+    const currentTime = utcToZonedTime(new Date(), weather.tzString);
+    const time = Math.trunc(currentTime.getTime() / 1000); // convert millis to seconds
+
+
+    return { temperature: weather.temperature, conditions: weather.conditions, time };
+}
+
 const getAllWeathers = async () => {
     const weathers = await Promise.all(locations.map(async (location) => {
-        const { city, latitude, longitude } = location;
-        const result = await getWeather(latitude, longitude);
+        const { city } = location;
+        const result = await getWeather(location);
         return {city, ...result};
     }));
     return weathers;
+}
+
+const warmCache = () => {
+    console.log('warming cache');
+    locations.forEach((location) => {
+        getWeather(location);
+    });
+}
+
+if (process.env.USE_CACHEWARMER) {
+    warmCache();
+    setInterval(warmCache, 30 * 1000);
 }
 
 export default getAllWeathers;
