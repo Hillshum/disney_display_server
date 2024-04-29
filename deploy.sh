609c74e5514ad37b954f47f2c929e9c52c4cf6b7
Hilton Shumway <hillshum@gmail.com>
2024-04-29 16:00:51 -0500

    Update to new server
    Switch to an ARM image for RPi and eliminate reboot script call (haven't set that up)

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker build -t hilllshum/disney_display_server . && docker push hilllshum/disney_display_server && ssh thinky 'sudo /home/hillshum/restart_disney.sh'
+docker build -t registry.hillshum.com/disney_display_server . && docker push registry.hillshum.com/disney_display_server # && ssh thinky 'sudo /home/hillshum/restart_disney.sh'
